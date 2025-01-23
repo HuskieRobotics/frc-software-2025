fb3f41b077eacc120e4c159328ddb4d3afe820a1
james wu <user769@yahoo.com>
2025-01-22 19:45:11 -0600

    added some to removal i got to leave bye

--- src/main/java/frc/robot/subsystems/removal/Removal.java
+++ src/main/java/frc/robot/subsystems/removal/Removal.java
@@ -1,15 +1,23 @@
 package frc.robot.subsystems.removal;
 
+import org.littletonrobotics.junction.Logger;
 
 
-public class Removal extends SubsystemBase{
+public class Removal extends SubsystemBase {
     private RemovalIO io;
+
+    private final RemovalIOInputsAutoLogged removalInputs = new RemovalIOInputsAutoLogged();
     
     public Removal(RemovalIO io){
         this.io = io;
     }
 
     private void rollRoller() {
-        io.setRollerMotorVoltage(REMOVAL_VOLTAGE_ROLLERS);
+        io.setRollerMotorVoltage(RemovalConstants.REMOVAL_VOLTAGE_ROLLERS);
     }
+}
+
+@Override
+public void periodic() {
+    io.updateInputs(inputs);
 }
