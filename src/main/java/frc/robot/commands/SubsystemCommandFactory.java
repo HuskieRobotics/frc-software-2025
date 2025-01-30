695c4cda6dbdc05bbaa20260a463ca07292d5e60
rina-xu <rjxu@stu.naperville203.org>
2025-01-29 18:40:47 -0600

    started subsystem commands

--- src/main/java/frc/robot/commands/SubsystemCommandFactory.java
+++ src/main/java/frc/robot/commands/SubsystemCommandFactory.java
@@ -2,6 +2,8 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.lib.team3061.drivetrain.Drivetrain;
+import frc.robot.subsystems.Climber.Climber;
 import frc.robot.subsystems.subsystem.Subsystem;
 
 public class SubsystemCommandFactory {
@@ -17,4 +19,16 @@ public class SubsystemCommandFactory {
   public Command getExampleSubsystemCommand(Subsystem subsystem) {
     return Commands.runOnce(subsystem::exampleMethod, subsystem);
   }
+
+  public Command getPrepClimbSequence(Drivetrain drivetrain, Climber climber) {
+    // require x subsystem to do __ (implement later)
+    // ignore need for drivetrain for now
+    // retract climber all the way (maybe)
+  }
+  
+  public Command getInitiateClimbButton(Drivetrain drivetrain, Climber climber) {
+    // 
+
+  }
+
 }
