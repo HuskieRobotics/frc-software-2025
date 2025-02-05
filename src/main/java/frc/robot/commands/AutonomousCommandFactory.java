3fe5c6e295c6df9aa6fb35d2bff6457f4446598b
Stefan Ilic <stefan.ilic314@gmail.com>
2025-02-04 19:33:48 -0600

    add placeholder auto commands

--- src/main/java/frc/robot/commands/AutonomousCommandFactory.java
+++ src/main/java/frc/robot/commands/AutonomousCommandFactory.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.lib.team3061.drivetrain.Drivetrain;
@@ -72,5 +73,36 @@ public class AutonomousCommandFactory {
             Commands.deadline(
                 Commands.waitSeconds(0.5),
                 Commands.run(() -> drivetrain.drive(0.1, -0.1, 0.0, true, false), drivetrain))));
+  }
+
+  // When programmed, each score coral command will drive to the specified pose on the reef and then score the coral
+
+  private static Command getScoreJL4Command() {
+    return Commands.waitSeconds(1);
+  }
+
+  private static Command getScoreKL4Command() {
+    return Commands.waitSeconds(1);
+  }
+
+  private static Command getScoreEL4Command() {
+    return Commands.waitSeconds(1);
+  }
+  
+  private static Command getScoreDL4Command() {
+    return Commands.waitSeconds(1);
+  }
+
+  // when programmed, this will wait until a coral is fully detected within the robot (use manipulator state machine)
+  private static Command getCollectCoralCommand() {
+    return Commands.waitSeconds(1);
+  }
+
+  public static void configureAutoCommands() {
+    NamedCommands.registerCommand("Score J L4", getScoreJL4Command());
+    NamedCommands.registerCommand("Score K L4", getScoreKL4Command());
+    NamedCommands.registerCommand("Score E L4", getScoreEL4Command());
+    NamedCommands.registerCommand("Score D L4", getScoreDL4Command());
+    NamedCommands.registerCommand("Collect Coral", getCollectCoralCommand());
   }
 }
