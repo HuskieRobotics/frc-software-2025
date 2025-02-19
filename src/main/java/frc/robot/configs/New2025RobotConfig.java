a18b34b3e47ff11f701f864069230c817f388fce
Stefan Ilic <111461725+StefanIlic1@users.noreply.github.com>
2025-02-19 16:42:36 -0600

    update max velocity

--- src/main/java/frc/robot/configs/New2025RobotConfig.java
+++ src/main/java/frc/robot/configs/New2025RobotConfig.java
@@ -76,7 +76,7 @@ public class New2025RobotConfig extends RobotConfig {
   private static final double DRIVE_KV = 0.072502;
   private static final double DRIVE_KA = 0.43636;
 
-  private static final LinearVelocity MAX_VELOCITY = MetersPerSecond.of(5.0); // actually 4.16
+  private static final LinearVelocity MAX_VELOCITY = MetersPerSecond.of(4.67);
   private static final LinearVelocity MAX_COAST_VELOCITY =
       MetersPerSecond.of(0.04); // FIXME: determine with real robot
   private static final double SLOW_MODE_MULTIPLIER = 0.7;
