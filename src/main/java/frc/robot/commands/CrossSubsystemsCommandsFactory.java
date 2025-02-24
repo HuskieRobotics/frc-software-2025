2bc3632d0350eb36ac65ec4e72d6540c1648609c
Geoff Schmit <gcschmit@gmail.com>
2025-02-24 17:43:42 -0600

    Fix Level 1 scoring to not raise elevator.

--- src/main/java/frc/robot/commands/CrossSubsystemsCommandsFactory.java
+++ src/main/java/frc/robot/commands/CrossSubsystemsCommandsFactory.java
@@ -28,32 +28,41 @@ public class CrossSubsystemsCommandsFactory {
       Vision vision) {
     oi.getScoreCoralButton()
         .onTrue(
-            Commands.sequence(
-                    Commands.either(
-                        Commands.parallel(
-                            Commands.runOnce(manipulator::removeAlgae),
-                            Commands.sequence(
-                                getScoreCoralCommand(manipulator),
-                                Commands.runOnce(elevator::goBelowSelectedAlgaePosition, elevator),
-                                Commands.waitUntil(elevator::isBelowSelectedAlgaePosition),
-                                new DriveToPose(
-                                    drivetrain,
-                                    () -> Field2d.getInstance().getNearestBranch(Side.REMOVE_ALGAE),
-                                    manipulator::setReadyToScore,
-                                    new Transform2d(
-                                        Units.inchesToMeters(2.0),
-                                        Units.inchesToMeters(1.0),
-                                        Rotation2d.fromDegrees(2.0)),
+            Commands.either(
+                    Commands.sequence(
+                        Commands.runOnce(manipulator::scoreCoralThroughFunnel, manipulator),
+                        Commands.waitUntil(manipulator::isWaitingForCoral)),
+                    Commands.sequence(
+                        Commands.either(
+                            Commands.parallel(
+                                Commands.runOnce(manipulator::removeAlgae),
+                                Commands.sequence(
+                                    getScoreCoralCommand(manipulator),
+                                    Commands.runOnce(
+                                        elevator::goBelowSelectedAlgaePosition, elevator),
+                                    Commands.waitUntil(elevator::isBelowSelectedAlgaePosition),
+                                    new DriveToPose(
+                                        drivetrain,
+                                        () ->
+                                            Field2d.getInstance()
+                                                .getNearestBranch(Side.REMOVE_ALGAE),
+                                        manipulator::setReadyToScore,
+                                        new Transform2d(
+                                            Units.inchesToMeters(2.0),
+                                            Units.inchesToMeters(1.0),
+                                            Rotation2d.fromDegrees(2.0)),
                                         0.5),
-                                Commands.runOnce(elevator::goAboveSelectedAlgaePosition, elevator),
-                                Commands.waitUntil(elevator::isAboveSelectedAlgaePosition),
-                                Commands.waitSeconds(0.5),
-                                Commands.runOnce(manipulator::algaeIsRemoved))),
-                        getScoreCoralCommand(manipulator),
-                        elevator::isAlgaePositionSelected),
-                    Commands.runOnce(
-                        () -> elevator.goToPosition(ElevatorConstants.ReefBranch.HARDSTOP),
-                        elevator))
+                                    Commands.runOnce(
+                                        elevator::goAboveSelectedAlgaePosition, elevator),
+                                    Commands.waitUntil(elevator::isAboveSelectedAlgaePosition),
+                                    Commands.waitSeconds(0.5),
+                                    Commands.runOnce(manipulator::algaeIsRemoved))),
+                            getScoreCoralCommand(manipulator),
+                            elevator::isAlgaePositionSelected),
+                        Commands.runOnce(
+                            () -> elevator.goToPosition(ElevatorConstants.ReefBranch.HARDSTOP),
+                            elevator)),
+                    () -> OISelector.getOperatorInterface().getLevel1Trigger().getAsBoolean())
                 .withName("score coral"));
 
     // drive to left branch of nearest reef face
@@ -70,7 +79,7 @@ public class CrossSubsystemsCommandsFactory {
                                 Units.inchesToMeters(2.0),
                                 Units.inchesToMeters(0.5),
                                 Rotation2d.fromDegrees(2.0)),
-                                5.0),
+                            5.0),
                         Commands.runOnce(
                             () -> vision.specifyCamerasToConsider(List.of(0, 1, 2, 3)))),
                     Commands.runOnce(elevator::goToSelectedPosition, elevator))
@@ -90,7 +99,7 @@ public class CrossSubsystemsCommandsFactory {
                                 Units.inchesToMeters(2.0),
                                 Units.inchesToMeters(0.5),
                                 Rotation2d.fromDegrees(2.0)),
-                                5.0),
+                            5.0),
                         Commands.runOnce(
                             () -> vision.specifyCamerasToConsider(List.of(0, 1, 2, 3)))),
                     Commands.runOnce(elevator::goToSelectedPosition, elevator))
@@ -103,12 +112,7 @@ public class CrossSubsystemsCommandsFactory {
 
   private static Command getScoreCoralCommand(Manipulator manipulator) {
     return Commands.sequence(
-        Commands.either(
-            Commands.sequence(
-                Commands.runOnce(manipulator::scoreCoralThroughFunnel, manipulator),
-                Commands.waitUntil(manipulator::isWaitingForCoral)),
-            Commands.runOnce(manipulator::shootCoral, manipulator),
-            () -> OISelector.getOperatorInterface().getLevel1Trigger().getAsBoolean()),
+        Commands.runOnce(manipulator::shootCoral, manipulator),
         Commands.waitUntil(() -> !manipulator.hasCoral()));
   }
 
