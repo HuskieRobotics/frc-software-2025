20a64f64efdb8e8a8f5744b8139fa3f42237de9f
Geoff Schmit <gcschmit@gmail.com>
2025-10-13 14:48:26 -0500

    Add delay when scoring L4 to accommodate elevator slop.

--- src/main/java/frc/robot/commands/CrossSubsystemsCommandsFactory.java
+++ src/main/java/frc/robot/commands/CrossSubsystemsCommandsFactory.java
@@ -170,6 +170,7 @@ public class CrossSubsystemsCommandsFactory {
         Commands.sequence(
             Commands.runOnce(elevator::goToSelectedPosition, elevator),
             Commands.waitUntil(elevator::isAtSelectedPosition),
+            Commands.waitSeconds(0.5),
             Commands.runOnce(manipulator::shootCoralFast, manipulator),
             Commands.waitUntil(() -> !manipulator.coralIsInManipulator())),
         () -> OISelector.getOperatorInterface().getLevel1Trigger().getAsBoolean());
@@ -438,6 +439,7 @@ public class CrossSubsystemsCommandsFactory {
         Commands.either(
             getScoreCoralAndCollectAlgaeCommand(drivetrain, manipulator, elevator, vision),
             Commands.sequence(
+                Commands.waitSeconds(0.5),
                 Commands.runOnce(manipulator::shootCoralFast, manipulator),
                 Commands.waitUntil(() -> !manipulator.coralIsInManipulator()),
                 Commands.runOnce(() -> elevator.setXFromReef(100.0))),
